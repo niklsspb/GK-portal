5ad794a516512a449042297262998aca0212748a
YURY TILMAN <uatilman@gmail.com>
2019-01-18 20:49:47 +0300

    Добавлен пустой spring-boot проект c зависимостями

---
+++ src/main/java/ru/geekbrains/gkportal/GkPortalApplication.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.gkportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GkPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GkPortalApplication.class, args);
+	}
+
+}
+
