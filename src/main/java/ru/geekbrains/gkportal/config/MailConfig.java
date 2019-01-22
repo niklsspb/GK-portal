8d5a8816c7ee7755951104b003852af0af7e146d
uatilman <uatilman@gmail.com>
2019-01-22 22:20:06 +0300

    Add e-mail send module

---
+++ src/main/java/ru/geekbrains/gkportal/config/MailConfig.java
@@ -0,0 +1,45 @@
+package ru.geekbrains.gkportal.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import ru.geekbrains.gkportal.entities.PropertyType;
+import ru.geekbrains.gkportal.services.PropertyService;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    private PropertyService propertyService;
+
+    @Autowired
+    public void setPropertyService(PropertyService propertyService) {
+        this.propertyService = propertyService;
+    }
+
+    private String getPropertyValue(String propertyName) {
+        return propertyService.getPropertyValue(propertyName, PropertyType.MAIL);
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setDefaultEncoding(getPropertyValue("default_encoding"));
+        mailSender.setHost(getPropertyValue("host"));
+        mailSender.setPort(Integer.parseInt(getPropertyValue("port")));
+        mailSender.setUsername(getPropertyValue("user_name"));
+        mailSender.setPassword(getPropertyValue("password"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", getPropertyValue("transport_protocol"));
+        props.put("mail.smtp.auth", getPropertyValue("smtp_auth"));
+        props.put("mail.smtp.starttls.enable", getPropertyValue("smtp_starttls_enable"));
+        props.put("mail.debug", getPropertyValue("debug"));
+
+        return mailSender;
+    }
+}
