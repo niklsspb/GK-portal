a09ec9ddf922c2b4d617828e643a201fcd0b50c7
rinat <renanew@mail.ru>
2019-01-26 19:56:41 +0500

    update scripts and data change data type column id to char(32)

--- src/main/resources/db/migration/V3_1__add_property_table.sql
+++ src/main/resources/db/migration/V3_1__add_property_table.sql
@@ -1,9 +1,8 @@
 
-
 DROP TABLE IF EXISTS `properties`;
 
 CREATE TABLE `properties` (
-  `property_id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `property_id` char(32) NOT NULL,
   `name`        VARCHAR(255)     NOT NULL,
   `value`       VARCHAR(255)     NOT NULL,
   `type`        VARCHAR(45)      NOT NULL,
