c0c21b239b6954b5ed8d331c8c06c74cab25b602
karelalex <karelalex83@gmail.com>
2019-01-26 16:43:35 +0300

    Added new files to work with Porches without Floor

--- src/main/java/ru/geekbrains/gkportal/services/HouseService.java
+++ src/main/java/ru/geekbrains/gkportal/services/HouseService.java
@@ -2,9 +2,7 @@ package ru.geekbrains.gkportal.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.geekbrains.gkportal.DTO.Floor;
-import ru.geekbrains.gkportal.DTO.House;
-import ru.geekbrains.gkportal.DTO.Porch;
+import ru.geekbrains.gkportal.DTO.*;
 import ru.geekbrains.gkportal.entities.BuildPorchConfig;
 import ru.geekbrains.gkportal.entities.Flat;
 import ru.geekbrains.gkportal.repository.BuildPorchConfigRepository;
@@ -34,17 +32,33 @@ public class HouseService {
         House house = new House();
         house.setNumber(houseNumber);
         for (BuildPorchConfig config : porchConfigs
-             ) {
-            Porch porch= new Porch();
+        ) {
+            Porch porch = new Porch();
             porch.setDescription(config);
-            for(int i=config.getFloorsCount(); i>=1; i--){
-                Floor floor= new Floor();
+            for (int i = config.getFloorsCount(); i >= 1; i--) {
+                Floor floor = new Floor();
                 int w = i;
                 floor.setNumber(w);
-                flats.parallelStream().filter(f->f.getFloor()==w).filter(f->f.getPorch()==config.getPorchID())
+                flats.parallelStream().filter(f -> f.getFloor() == w).filter(f -> f.getPorch() == config.getPorchID())
                         .forEach(floor.getFlats()::add);
                 porch.getFloors().add(floor);
             }
+            house.getPorchList().add(porch);
+        }
+        return house;
+    }
+
+    public HouseSimple buildSimple(int houseNumber) {
+        List<BuildPorchConfig> porchConfigs = porchConfigRepository.findAllByhousingID(houseNumber);
+        List<Flat> flats = flatRepository.findAllByHouse(houseNumber);
+        HouseSimple house = new HouseSimple();
+        house.setNumber(houseNumber);
+        for (BuildPorchConfig config : porchConfigs)
+        {
+            PorchSimple porch = new PorchSimple();
+            porch.setDescription(config);
+            flats.stream().filter(f -> f.getPorch() == config.getPorchID())
+                    .forEach(porch.getFlats()::add);
             house.getPorchList().add(porch);
         }
         return house;
