c7d60247a0d1171dbe427589fe4497b5f7006a86
karelalex <karelalex83@gmail.com>
2019-01-27 22:10:08 +0300

    Now flat movement is working

--- src/main/java/ru/geekbrains/gkportal/controllers/HouseRestController.java
+++ src/main/java/ru/geekbrains/gkportal/controllers/HouseRestController.java
@@ -2,6 +2,7 @@ package ru.geekbrains.gkportal.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import ru.geekbrains.gkportal.DTO.FlatDTO;
 import ru.geekbrains.gkportal.services.HouseService;
@@ -19,7 +20,8 @@ public class HouseRestController {
         this.houseService = houseService;
     }
 
-    @PutMapping("/flat")
+
+    @PutMapping(value = "/flat")
     public int changeFlat (@RequestBody FlatDTO flatDTO) {
         houseService.changeFlat(flatDTO);
         return HttpStatus.OK.value();
