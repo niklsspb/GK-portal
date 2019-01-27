5fc142716b3c9deeb547fcd84ee2a11509e4bc63
karelalex <karelalex83@gmail.com>
2019-01-27 18:34:32 +0300

    Splited registration form and editing of house

---
+++ src/main/resources/static/js/editboard.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    $('.gk-flat').click(function () {
+        const flatId = $(this).attr('data-flat-id');
+        const flatNumber = $(this).text();
+        $('#flatID').val(flatId);
+        $('#flatNumber').text(flatNumber);
+    });
+    $('#edit').click(function () {
+        $('.gk-flat-holder').sortable({
+            items: '> .gk-flat',
+            connectWith: '.gk-flat-holder',
+            update: function (event, ui) {
+                // var posX = ui.item.parent().children().index(ui.item);
+                $('.gk-flat-holder').each(function () {
+                    if ($(this).children().length > 1) {
+                        $(this).css({
+                            'background-color': 'red',
+                            'border-color' : 'red'
+                        })
+                    } else {
+                        $(this).css({
+                            'background-color': 'inherit'
+                        })
+                    }
+                });
+            },
+        }).disableSelection;
+    })
+
+});
+
