817b0e41e8a6221565d4cee019322026885d59fc
Alexander Karelin <35873006+karelalex@users.noreply.github.com>
2019-01-27 19:12:35 +0300

    Update Property.java

--- src/main/java/ru/geekbrains/gkportal/entities/Property.java
+++ src/main/java/ru/geekbrains/gkportal/entities/Property.java
@@ -11,7 +11,7 @@ public class Property {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "property_id")
-    private Long id;
+    private String id;
 
     @Column(name = "name")
     private String name;
