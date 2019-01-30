91ab5a3a55b9add676bce66ebd1526a558d58869
Ivanez81 <32313325+Ivanez81@users.noreply.github.com>
2019-01-30 12:47:44 +0300

    Correction of repositories for using String instead of UUID type.

--- src/main/java/ru/geekbrains/gkportal/repository/ContactTypeRepository.java
+++ src/main/java/ru/geekbrains/gkportal/repository/ContactTypeRepository.java
@@ -4,9 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.geekbrains.gkportal.entities.ContactType;
 
-import java.util.UUID;
-
 @Repository
-public interface ContactTypeRepository extends JpaRepository<ContactType, UUID> {
+public interface ContactTypeRepository extends JpaRepository<ContactType, String> {
 
 }
