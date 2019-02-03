b979b35a4370267df33d53d17c5826246e564be4
RinatShangareev <ShangareevRR@mail.ru>
2019-02-03 18:55:02 +0500

    refactoring systemAccount and validation not equal password, annotation @Email

--- src/main/java/ru/geekbrains/gkportal/validation/PhoneNumberValidator.java
+++ src/main/java/ru/geekbrains/gkportal/validation/PhoneNumberValidator.java
@@ -6,16 +6,18 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class PhoneNumberValidator implements ConstraintValidator<ValidPhoneNumber, String> {
+    private static final String PHONENUMBER_PATTERN = "^$|[0-9]{10}";
     private Pattern pattern;
     private Matcher matcher;
-    private static final String PHONENUMBER_PATTERN = "^$|[0-9]{10}";
 
     @Override
     public boolean isValid(final String phoneNumber, final ConstraintValidatorContext context) {
+
         pattern = Pattern.compile(PHONENUMBER_PATTERN);
         if (phoneNumber == null) {
             return false;
         }
+
         matcher = pattern.matcher(phoneNumber);
         return matcher.matches();
     }
