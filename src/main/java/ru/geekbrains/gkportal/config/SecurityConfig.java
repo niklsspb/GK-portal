a1fde739a9f379be2d0609f8a9a24a4a7c6c2713
Ivanez81 <32313325+Ivanez81@users.noreply.github.com>
2019-02-05 15:44:20 +0300

    Added developers section, JNC corrections.

--- src/main/java/ru/geekbrains/gkportal/config/SecurityConfig.java
+++ src/main/java/ru/geekbrains/gkportal/config/SecurityConfig.java
@@ -10,7 +10,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import ru.geekbrains.gkportal.services.AccountService;
+import ru.geekbrains.gkportal.service.AccountService;
 
 /**
  * Аннотация @EnableGlobalMethodSecurity позволяет использовать аннотации для защиты на уровне
---
+++ src/main/java/ru/geekbrains/gkportal/entity/Property.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.gkportal.entity;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Data
+@Entity(name = "properties")
+@EqualsAndHashCode(callSuper = true)
+public class Property extends AbstractEntity {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "value")
+    private String value;
+
+    @Column(name = "type")
+    @Enumerated(EnumType.STRING)
+    private PropertyType propertyType;
+
+    public Property() {
+    }
+}
