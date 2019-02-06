8431f12a9fd0b0932be69203df89e7a594d6ff07
Degtyar <degtyar.ks@ya.ru>
2019-02-06 17:20:31 +0700

    Add Personal Area  (#26)
    * Personal Area add info Flat and Communication
    
    * Delete V4_2__create_view_function_to_view_contact.sql

--- src/main/java/ru/geekbrains/gkportal/repository/FlatRepository.java
+++ src/main/java/ru/geekbrains/gkportal/repository/FlatRepository.java
@@ -11,7 +11,8 @@ import java.util.Optional;
 @Repository
 public interface FlatRepository extends JpaRepository<Flat, String> {
     List<Flat> findAllByHouse(int house);
-    List<Flat> findAllByHouseAndPorch(int house, int porch);
-    Optional<Flat> findByHouseAndPorchAndFloorAndFlatNumber(int house, int porch, int floor, int flatNumber);
 
+    List<Flat> findAllByHouseAndPorch(int house, int porch);
+
+    Optional<Flat> findByHouseAndPorchAndFloorAndFlatNumber(int house, int porch, int floor, int flatNumber);
 }
