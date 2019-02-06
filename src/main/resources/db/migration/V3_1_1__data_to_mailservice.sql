68d527db90f754cb63ee6ac59b0d4bf09aecc772
uatilman <uatilman@gmail.com>
2019-02-06 08:38:39 +0300

    Revert Comment properties data which migrate to properties.

--- src/main/resources/db/migration/V3_1_1__data_to_mailservice.sql
+++ src/main/resources/db/migration/V3_1_1__data_to_mailservice.sql
@@ -2,7 +2,7 @@ INSERT INTO `support_boot_db`.`properties` (`id`, `name`, `value`,`type`) VALUES
 ('cad1d71f-2166-11e9-a2f1-3085a9986b2c', 'host', 'smtp.gmail.com', 'MAIL'),
 ('d565eb4f-2166-11e9-a2f1-3085a9986b2c', 'port', '587', 'MAIL'),
 ('dd53215b-2166-11e9-a2f1-3085a9986b2c', 'user_name', 'd107ru@gmail.com', 'MAIL'),
--- ('e9842295-2166-11e9-a2f1-3085a9986b2c', 'password', '', 'MAIL'),
+('e9842295-2166-11e9-a2f1-3085a9986b2c', 'password', '', 'MAIL'),
 ('ef9e0fea-2166-11e9-a2f1-3085a9986b2c', 'smtp_auth', 'true', 'MAIL'),
 ('f48ed3a3-2166-11e9-a2f1-3085a9986b2c', 'smtp_starttls_enable', 'true', 'MAIL'),
 ('f8f17c29-2166-11e9-a2f1-3085a9986b2c', 'transport_protocol', 'smtp', 'MAIL'),
