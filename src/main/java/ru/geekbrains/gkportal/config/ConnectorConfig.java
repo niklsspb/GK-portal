494b8ba3e64dc9c3942b4a4772410325d0e29e2b
chertenokru <admin@chertenok.ru>
2019-02-14 08:28:39 +0300

    fix

---
+++ src/main/java/ru/geekbrains/gkportal/config/ConnectorConfig.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.gkportal.config;
+
+
+
+/*
+//@Configuration
+public class ConnectorConfig {
+
+    @Bean
+    public TomcatServletWebServerFactory containerFactory() {
+        return new TomcatServletWebServerFactory() {
+            protected void customizeConnector(Connector connector) {
+                int maxSize = 50000000;
+                super.customizeConnector(connector);
+                connector.setMaxPostSize(maxSize);
+                connector.setMaxSavePostSize(maxSize);
+                if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
+
+                    ((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(maxSize);
+                    logger.info("Set MaxSwallowSize " + maxSize);
+                }
+            }
+        };
+
+    }
+
+
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
+}
+*/
