3f510bc04939853a4533081995dd506901cf106f
chertenokru <admin@chertenok.ru>
2019-02-15 20:30:10 +0300

    crete view .FIX

--- src/main/resources/db/migration/V4_7__create_view_question_result.sql
+++ src/main/resources/db/migration/V4_7__create_view_question_result.sql
@@ -5,7 +5,7 @@ DELIMITER $$
  DROP VIEW IF EXISTS `questionnaire_result_view`$$
 
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`support_boot`@`localhost` SQL SECURITY DEFINER VIEW `questionnaire_result_view` AS (
+CREATE  VIEW `questionnaire_result_view` AS (
 
 SELECT r.questionnaire_id,`r`.`question_id`,`r`.`answer_id`,
 MIN(`q`.`name`) AS `question_name`,
