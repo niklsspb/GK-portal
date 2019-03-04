df94fb24d5a930a69debecef38012f5e05e0946f
Vladimir <chertenokru@users.noreply.github.com>
2019-03-04 15:49:20 +0300

    moved to google pieDiograms (#59)

--- src/main/resources/static/js/pie.js
+++ src/main/resources/static/js/pie.js
@@ -1,22 +1,27 @@
 $(document).ready(function () {
-    $('.question-container').each(function () {
-        var dataSet = new Array();
-        $(this).find('tbody tr').each(function () {
-            var answer = $(this).find('.answer-name').text();
-            var count = $(this).find('.answer-count').text();
-            var dataItem = {label: answer, data: count};
-            dataSet.push(dataItem);
-        });
-        var targetdiv=$(this).find('.diogram');
-        var options = {
-            series: {
-                pie: {
-                    show: true
-                }
-            }
-        };
-        console.log(dataSet);
-        $.plot(targetdiv, dataSet, options);
-        //$(this).find('table').css('display' , 'none');
-    })
+    google.charts.load("current", {packages:["corechart"]});
+    google.charts.setOnLoadCallback(function () {
+        $('.question-container').each(function () {
+            var data = new google.visualization.DataTable();
+            data.addColumn('string', 'Answer');
+            data.addColumn('number', 'Count');
+            var question=$(this).find('p').text();
+            $(this).find('tbody tr').each(function () {
+                var answer = $(this).find('.answer-name').text();
+                var count = parseInt($(this).find('.answer-count').text());
+                data.addRows([[answer, count]]);
+            });
+            var targetdiv=$(this).find('.diogram').get(0);
+            var options = {
+                title: question
+            };
+            var chart = new google.visualization.PieChart(targetdiv);
+            chart.draw(data, options);
+            $(this).find('p').hide();
+            $(this).find('table').hide();
+
+            //$(this).find('table').css('display' , 'none');
+        })
+    });
+
 })
