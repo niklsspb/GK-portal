cc1d019563257035745200e69e92246d1778be54
chertenokru <admin@chertenok.ru>
2019-03-13 10:06:31 +0300

    Correct FlatToFlat entity and sql script

--- src/main/java/ru/geekbrains/gkportal/entity/FlatToFlatProperty.java
+++ src/main/java/ru/geekbrains/gkportal/entity/FlatToFlatProperty.java
@@ -19,8 +19,11 @@ public class FlatToFlatProperty extends AbstractEntity {
     @OneToOne
     @JoinColumn(name = "flat_property_id")
     private FlatProperty flatProperty;
-    @Column(name = "date_create")
+    @Column(name = "date_change")
     private Date createDate;
+    @OneToOne
+    @JoinColumn(name = "changed_by_contact_id")
+    private Contact changedByContact;
     @Column
     private String comment;
     @Column
