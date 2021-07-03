8e97646c360bb1f5f90159639d6066e2e5996617
Владимир К <admin@chertenok.ru>
2021-07-03 23:36:10 +0300

    fix in text, fix migration 4.8 - add encoding table

--- src/main/resources/db/migration/V4_8__create_table_questionnaire_confirmed_type.sql
+++ src/main/resources/db/migration/V4_8__create_table_questionnaire_confirmed_type.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS `questionnaire_confirmed_type`;
 
-CREATE TABLE `support_boot_db`.`questionnaire_confirmed_type` (
+CREATE TABLE `questionnaire_confirmed_type` (
   `id` CHAR(36) NOT NULL,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`));
+  PRIMARY KEY (`id`))
+  ENGINE=InnoDB DEFAULT CHARSET=utf8;
